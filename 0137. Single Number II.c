89363360164055970b62326bbbb968911f9da6ea
Hui-Hua, Hsiao <94283307+HHHJustin@users.noreply.github.com>
2023-08-17 13:51:35 +0800

    Update 0137. Single Number II.c

--- 0137. Single Number II.c
+++ 0137. Single Number II.c
@@ -2,11 +2,14 @@ int singleNumber(int* nums, int numsSize){
   int sum = 0;
   for(int i = 0; i < 32; i++){
       int temp = 0;
+     // 紀錄每個位元1的總數   
       for(int j = 0; j < numsSize;j++){
           temp += (nums[j] >> i) & 1;
+        // 位移每個位元且and 1獲取第一位元  
       }
       temp %= 3;
       sum += (temp << i);
+     // 將bit轉換回去後加總   
   }
   return sum;
 }
@@ -18,4 +21,3 @@ int singleNumber(int* nums, int numsSize){
 // +      0010
 // -----------
 //        0041 -> % 3 -> 0011 -> 3
-
