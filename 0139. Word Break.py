ad1f8c392a3955cc83623b6a8f655fabe4e04467
Hui-Hua, Hsiao <94283307+HHHJustin@users.noreply.github.com>
2023-10-14 21:23:26 +0800

    Update 0139. Word Break.py

--- 0139. Word Break.py
+++ 0139. Word Break.py
@@ -5,8 +5,11 @@ class Solution:
 
         for index in range(1, len(s) + 1):
             for j in range(index):
+                # dp[j] represent the s[:j] can be segmented in wordDict
+                # s[j:index] in wordDict represent s[j:index] can be segmented in this time.
                 if dp[j] and s[j:index] in wordDict:
                     dp[index] = True
+                    break
         return dp[-1]
 
 # s ="leetcode", wordDict = ["leet", "code"]
@@ -20,6 +23,7 @@ class Solution:
 
 # Then we can the word in wordDict appear in the s. 
 # if the index (current - len(word)) == True -> 
-# represent the word can be segmented -> then dp[current index] = True
+# represent the word can be segmented and the string before can be segmented too.
+# -> then dp[current index] = True
 
 # finally, just to check the dp[-1] -> we can know the anwser
